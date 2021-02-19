65c2be22e4597fc09383c03604faaa20c9c18079
mdsazzad-hossain <mdsojib508@gmail.com>
2021-02-19 23:30:04 +0600

    add terbolinks

--- resources/views/livewire/backend/layouts/app.blade.php
+++ resources/views/livewire/backend/layouts/app.blade.php
@@ -428,11 +428,9 @@
         </aside>
 
         <footer class="main-footer">
-            <!-- To the right -->
             <div class="float-right d-none d-sm-inline">
                 Anything you want
             </div>
-            <!-- Default to the left -->
             <strong>Copyright © 2020 <a href="https://ideatechsolution.com">AdminPanel</a>.</strong> All rights
             reserved.
         </footer>
