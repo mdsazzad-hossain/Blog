adee5fcb441358840efe80426b8f716c1b14691e
mdsazzad-hossain <mdsojib508@gmail.com>
2021-03-09 22:03:08 +0600

    fetch data to frontend

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -1,15 +1,17 @@
 <?php
 
 namespace App\Http\Livewire;
-
+use App\Models\Demo;
 use Livewire\Component;
-
+use Illuminate\Support\Facades\Auth;
 class Home extends Component
 {
+    public $data,$demos;
     public function render()
     {
-        return view('livewire.home')
-        ->extends('frontend.app')
+        $this->data = auth()->user();
+        $this->demos = Demo::all();
+        return view('livewire.home')->extends('frontend.app')
         ->section('content');
     }
 }
